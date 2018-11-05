6fe3d36bd06095493c240c8e8fcf60c74705c1f6
Matt Burton <madeofburtonium@gmail.com>
2018-11-05 03:46:21 +0000

    prod configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyACigiUh7pjvwUzQDgQV81UpsDvb2Qf3aA",
+    authDomain: "ng-testdrive-prod.firebaseapp.com",
+    databaseURL: "https://ng-testdrive-prod.firebaseio.com",
+    projectId: "ng-testdrive-prod",
+    storageBucket: "ng-testdrive-prod.appspot.com",
+    messagingSenderId: "669260345329"
+  }
 };
